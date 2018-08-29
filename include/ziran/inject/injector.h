b27005b45cfb81b6f6aaa9c2356e3b0d96897ae2
NaturalSelect <2145973003@qq.com>
2018-08-29 17:16:05 +0800

    添加依赖注入容器

---
+++ include/ziran/inject/injector.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include <memory>
+#include <any>
+#include <unordered_map>
+#include <stdexcept>
+namespace ziran
+{
+	namespace injector
+	{
+		template<typename T>
+		class transient
+		{
+		public:
+			transient()=default;
+			~transient()=default;
+			static std::function<T()> make(std::function<T()> &&maker)
+			{
+				return maker;
+			}
+		private:
+
+		};
+
+		template<typename T>
+		class singleton
+		{
+		public:
+			singleton()=default;
+			~singleton()=default;
+			static std::function<T()> make(std::function<T()> &&maker)
+			{
+				T value = maker();
+				return [value]() 
+				{
+					return value;
+				};
+			}
+		private:
+		};
+
+		class service_collection
+		{
+		public:
+			service_collection()=default;
+			~service_collection()=default;
+			template<typename T,typename MakePolicy = transient<T>>
+			service_collection &register_type(std::function<T()> &&maker = []() { return T(); })
+			{
+				std::string key(typeid(T).name());
+				if (map.find(key) != std::end(map))
+				{
+					throw std::logic_error("该类型已被注册");
+				}
+				map.insert(std::make_pair(key,MakePolicy::make(std::move(maker))));
+				return *this;
+			}
+			template<typename T>
+			T resolve_type()
+			{
+				std::string key(typeid(T).name());
+				auto itera = map.find(key);
+				if (itera == std::end(map))
+				{
+					throw std::invalid_argument("该类型没有被注册");
+				}
+				std::function<T()> maker = std::any_cast<std::function<T()>>(itera->second);
+				return maker();
+			}
+		private:
+			std::unordered_map<std::string, std::any> map;
+		};
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,30 @@
 #include "ziran/net/pseudo_header.h"
 #include <iostream>
 #include "ziran/async/parallel.h"
+#include "ziran/inject/injector.h"
 #include <thread>
+
 int main()
 {
-	std::cout << sizeof(ziran::net::ip_header) << std::endl
-		<< sizeof(ziran::net::tcp_header) << std::endl
-		<< sizeof(ziran::net::udp_header) << std::endl
-		<< sizeof(ziran::net::pseudo_header) << std::endl;
-	auto res = ziran::async::parallel::invoke<int>({ 
-		[]() { std::this_thread::sleep_for(std::chrono::seconds(2));  return 1; }
-	,[]() {return 2; }
-	, []() {return 3; }
-	, []() {return 4; } 
+	ziran::injector::service_collection services;
+	services.register_type<int>()
+		.register_type<std::string, ziran::injector::singleton<std::string>>([]() 
+		{
+			return "abc";
 		});
+	int v1 = services.resolve_type<int>();
+	auto v2 = services.resolve_type<std::string>();
+	//ziran::inject::service<int, ziran::inject::life_time::singleton>::value = 10;
+	//std::cout << sizeof(ziran::net::ip_header) << std::endl
+	//	<< sizeof(ziran::net::tcp_header) << std::endl
+	//	<< sizeof(ziran::net::udp_header) << std::endl
+	//	<< sizeof(ziran::net::pseudo_header) << std::endl;
+	//auto res = ziran::async::parallel::invoke<int>({ 
+	//	[]() { std::this_thread::sleep_for(std::chrono::seconds(2));  return 1; }
+	//,[]() {return 2; }
+	//, []() {return 3; }
+	//, []() {return 4; } 
+	//	});
 	system("pause");
 	return 0;
 }
