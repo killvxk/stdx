1093c2ca251764192f6fa66771dee4a55226ae7d
NaturalSelect <2145973003@qq.com>
2018-08-30 09:26:11 +0800

    添加注释

--- include/ziran/event/event.h
+++ include/ziran/event/event.h
@@ -10,27 +10,32 @@ namespace ziran
 	{
 		//事件模板
 		template<
+			//参数类型
 			typename ...TArgs
 		>
 		class event
 		{
 		public:
+			//默认构造函数
 			event()=default;
+			//析构函数
 			~event()=default;
+			//move构造函数
 			event(event &&other)
 				:listeners(std::move(other.listeners))
 			{
 			}
-
+			//move赋值函数
 			event &operator=(event &&other)
 			{
 				listeners = std::move(other.listeners);
 			}
-
+			//注册监听器
 			void register_listener(std::function<void(TArgs...)> &&listener)
 			{
 				listeners.push_back(listener);
 			}
+			//发布事件
 			void pulish(const TArgs &...args)
 			{
 				ziran::async::parallel::for_each<std::function<void(TArgs...)>>(listeners, [&args...](const std::function<void(TArgs...)> &listener)
@@ -39,6 +44,7 @@ namespace ziran
 				});
 			}
 		private:
+			//监听器容器
 			std::vector<std::function<void(TArgs...)>> listeners;
 		};
 	}
