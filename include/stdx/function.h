6e3cad563da6fa1d4aedb36db9ffebc6bb5160c4
NaturalSelect <2145973003@qq.com>
2019-01-08 13:23:49 +0800

    更新C#风格函数对象

--- include/stdx/function.h
+++ include/stdx/function.h
@@ -3,12 +3,12 @@
 
 namespace stdx
 {
-	template<typename R = void>
+	template<typename _R = void>
 	class _BasicAction
 	{
 	public:
 		virtual ~_BasicAction() = default;
-		virtual R run() = 0;
+		virtual _R run() = 0;
 	};
 
 	template<typename _t, typename _fn>
@@ -30,12 +30,12 @@ namespace stdx
 		}
 	};
 
-	template<typename R,typename _Fn>
-	class _Action:public _BasicAction<R>
+	template<typename _R,typename _Fn>
+	class _Action:public _BasicAction<_R>
 	{
 	public:
 		_Action()
-			:_BasicAction()
+			:_BasicAction<_R>()
 		{}
 		_Action(_Fn &&fn)
 			:_BasicAction()
@@ -43,25 +43,25 @@ namespace stdx
 		{}
 		~_Action()=default;
 
-		_Action(_Action<R,_Fn> &&other)
+		_Action(_Action<_R,_Fn> &&other)
 			:m_func(std::move(other.m_func))
 		{
 		}
 
-		_Action(const _Action<R,_Fn> &other)
+		_Action(const _Action<_R,_Fn> &other)
 			:m_func(other.m_func)
 		{}
 
-		_Action<R,_Fn> &operator=(const _Action<R,_Fn> &other)
+		_Action<_R,_Fn> &operator=(const _Action<_R,_Fn> &other)
 		{
 			m_func = other.m_func;
 			return *this;
 		}
 
 		// Í¨¹ý _BasicAction ¼Ì³Ð
-		virtual R run() override
+		virtual _R run() override
 		{
-			return _ActionRunner<R,_Fn>::run(m_func);
+			return _ActionRunner<_R,_Fn>::run(m_func);
 		}
 
 		operator bool()
@@ -79,7 +79,7 @@ namespace stdx
 	public:
 		template<typename _Fn>
 		action(_Fn &&fn)
-			:m_impl(std::make_shared<_Action<_R,_Fn>>(fn))
+			:m_impl(new _Action<_R,_Fn>(std::move(fn)))
 		{}
 		action(const action<_R> &other)
 			:m_impl(other.m_impl)
@@ -90,12 +90,14 @@ namespace stdx
 		~action() = default;
 		action<_R> &operator=(const action<_R> &other)
 		{
+			printf("1");
 			m_impl = other.m_impl;
 			return *this;
 		}
 		_R operator()()
 		{
-			return m_impl->run();
+			m_impl->run();
+			return;
 		}
 		operator bool()
 		{
@@ -105,32 +107,73 @@ namespace stdx
 		impl_t m_impl;
 	};
 
-	template<typename R = void,typename _Fn>
-	action<R> make_action(_Fn &&func)
+	template<typename _R = void,typename _Fn>
+	action<_R> make_action(_Fn &&func)
 	{
-		return action<R>(func);
+		return action<_R>(func);
 	}
 
-	template<typename R = void, typename _Fn,typename ..._Args>
-	action<R> make_action(_Fn &&func,_Args &...args)
+	template<typename _R = void, typename _Fn,typename ..._Args>
+	action<_R> make_action(_Fn &&func,_Args &...args)
 	{
-		return action<R>(std::bind(func,args...));
+		return action<_R>(std::bind(func,args...));
 	}
 
-	template<typename R = void, typename _Fn>
-	std::shared_ptr<_Action<R, _Fn>> _MakeAction(_Fn &func)
+	template<typename _R = void, typename _Fn>
+	std::shared_ptr<_Action<_R, _Fn>> _MakeAction(_Fn &func)
 	{
-		return std::make_shared<_Action<R, _Fn>>(std::move(func));
+		return std::make_shared<_Action<_R, _Fn>>(std::move(func));
 	}
 
-	//template<typename _R,typename ..._Args>
-	//class function
-	//{
-	//public:
-	//	function();
-	//	~function();
-
-	//private:
-
-	//};
+	template<typename _R,typename ..._Args>
+	class _BasicFunction
+	{
+	public:
+		virtual ~_BasicFunction()=default;
+		virtual _R run(const _Args&...) = 0;
+	};
+	template<typename _R,typename _Fn,typename ..._Args>
+	class _Function:public _BasicFunction<_R,_Args...>
+	{
+	public:
+		_Function(_Fn &&fn)
+			:_BasicFunction<_R,_Args...>()
+			,m_fn(fn)
+		{}
+		~_Function() = default;
+		_R run(const _Args &...args) override
+		{
+			return std::invoke(m_fn,args...);
+		}
+	private:
+		_Fn m_fn;
+	};
+	template<typename _R,typename ..._Args>
+	class function
+	{
+		using impl_t = std::shared_ptr<_BasicFunction<_R,_Args...>>;
+	public:
+		template<typename _Fn>
+		function(_Fn &&fn)
+			:m_impl(new _Function<_R,_Fn,_Args...>(std::move(fn)))
+		{}
+		function(const function<_R,_Args...> &other)
+			:m_impl(other.m_impl)
+		{}
+		function(function<_R,_Args...> &&other)
+			:m_impl(std::move(other.m_impl))
+		{}
+		~function() = default;
+		function<_R, _Args...> &operator=(const function<_R, _Args...> &other)
+		{
+			m_impl = other.m_impl;
+			return *this;
+		}
+		_R operator()(const _Args &...args)
+		{
+			return m_impl->run(args...);
+		}
+	private:
+		impl_t m_impl;
+	};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,43 +8,15 @@
 
 int main()
 {
-	/*auto t = stdx::async<stdx::tuple<int, double, char>>([]()
+	stdx::function<void,int> action([](int i) 
 	{
-		stdx::tuple<int, double, char> t(10, 10, 'a');
-		return t;
-	}).then([](stdx::task_result<stdx::tuple<int, double, char>> r)
-	{
-		try
-		{
-			auto t = r.get();
-			std::cout << t.get<0>() << t.get<1>()<<t.get<2>();
-		}
-		catch (const std::exception&)
-		{
-			
-		}
-
-	}).then<stdx::task<void>>([](stdx::task_result<void> t) 
-	{
-		return stdx::async<void>([]() {});
-	}).then([](stdx::task_result<void>)
-	{
-
-	}).with(stdx::async([]() 
-	{
-	}));*/
-	auto t = stdx::async<int>([]() 
-	{
-		printf("1");
-		return 1;
-	})/*.then([](stdx::task_result<int> r) 
-	{
-		auto i = r.get();
-	}).with(stdx::async<void>([]() {}))
-		.then([](stdx::task_result<void> r) 
-	{
-		std::cout << "1";
-	})*/;
+		std::cout << 1 <<std::endl;
+	});
+	std::queue<stdx::function<void, int>> queue;
+	queue.push(action);
+	action = queue.front();
+	queue.pop();
+	action(1);
 	std::cin.get();
 	return 0;
 }
