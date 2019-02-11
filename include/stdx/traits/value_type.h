c9724fa36eb8ffd819d3f38cf1a2ad4db63f8076
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2019-02-11 15:22:17 +0800

    Create value_type.h

---
+++ include/stdx/traits/value_type.h
@@ -0,0 +1,22 @@
+#pragma once
+ 
+ namespace stdx
+ {
+     template<typename _T>
+     struct _ValueType
+     {
+         using type = _T;
+     };
+     template<typename _T>
+     struct _ValueType<_T&>
+     {
+         using type = _T;
+     };
+     template<typename _T>
+     struct _ValueType<_T&&>
+     {
+         using type = _T;
+     };
+     template<typename _T>
+     using value_type = _ValueType<_T>::type;
+ }
