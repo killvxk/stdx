55127b6c7bdfa53c2271d3c87fbcb04da615ffee
NaturalSelect <2145973003@qq.com>
2019-03-31 11:10:09 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,10 @@
 ﻿#include <stdx/io.h>
 #include <stdx/net/socket.h>
+#include <stdx/sys/window.h>
 #include <iostream>
 int main()
 {
-	/*stdx::file_io_service io_service;
-	stdx::async_file_stream stream(io_service,"e://test.txt",stdx::file_access_type::all,stdx::file_open_type::open,stdx::file_shared_model::shared_read);
-	try
-	{
-		std::string str = "23333333";
-		stream.write(str).then([&stream](stdx::file_write_event e) 
-		{
-			std::cout << "实际写入字节数" << e.size <<std::endl;
-			return stream.read(1024, 0);
-		})
-		.then([](stdx::file_read_event e) 
-		{
-			std::cout << "文件尾:" << e.eof <<std::endl
-						<<"文件内容:" << std::endl
-						<< e.buffer;
-		});
-	}
-	catch (const std::exception&e)
-	{
-		std::cerr << e.what();
-	}*/
-	stdx::network_io_service service;
+	/*stdx::network_io_service service;
 	stdx::socket s(service, stdx::addr_family::ip, stdx::socket_type::stream, stdx::protocol::tcp);
 	try 
 	{
@@ -36,7 +16,6 @@ int main()
 		std::cerr << e.what();
 		return -1;
 	}
-
 	s.listen(10);
 	auto c = s.accept();
 	c.recv(1024).then([c](stdx::network_recv_event e) mutable
@@ -46,68 +25,8 @@ int main()
 			{
 				c.close();
 			});
-	});
-	//try
-	//{
-	//	service.recv(c, 4096, [](stdx::network_recv_event e,std::exception_ptr error) {
-	//		if (error)
-	//		{
-	//			try
-	//			{
-	//				std::rethrow_exception(error);
-	//			}
-	//			catch (const std::exception&e)
-	//			{
-	//				std::cerr << e.what();
-	//			}
-	//		}
-	//		else
-	//		{
-	//			std::cout << e.buffer;
-	//		}
-	//	});
-	//}
-	//catch (const std::exception&e)
-	//{
-	//		std::cerr << e.what();
-	//		return 0;
-	//}
-
-	//try
-	//{
-	//	service.connect(s, addr);
-	//}
-	//catch (const std::exception&e)
-	//{
-	//	std::cerr << e.what();
-	//	return 0;
-	//}
-	//std::string str("hello world");
-	//try
-	//{
-	//	service.send(s, str.c_str(), str.size(), [](stdx::network_send_event e, std::exception_ptr error)
-	//	{
-	//		if (error)
-	//		{
-	//			try
-	//			{
-	//				std::rethrow_exception(error);
-	//			}
-	//			catch (const std::exception&e)
-	//			{
-	//				std::cerr << e.what();
-	//			}
-	//		}
-	//		else
-	//		{
-	//			std::cout << "send ok!";
-	//		}
-	//	});
-	//}
-	//catch (std::exception &e)
-	//{
-	//	std::cerr << e.what();
-	//}
+	});*/
+	stdx::message_box::show(NULL,"hello","body",stdx::message_button::ok,NULL);
 	std::cin.get();
 	return 0;
 }
