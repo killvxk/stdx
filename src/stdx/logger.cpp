be2af2f96d4bbc46e1d3af9e809f24f4b1ea3065
NaturalSelect <2145973003@qq.com>
2019-08-10 21:14:24 +0800

    update

--- src/stdx/logger.cpp
+++ src/stdx/logger.cpp
@@ -8,7 +8,7 @@ void stdx::_Logger::debug(cstring str)
 	SetConsoleTextAttribute(m_stderr, FOREGROUND_INTENSITY);
 	std::string string = "[DEBUG]";
 	string.append(str);
-	string.append(next_line);
+	string.append("\n");
 	puts(string.c_str());
 	SetConsoleTextAttribute(m_stderr, 0x07);
 }
@@ -18,7 +18,7 @@ void stdx::_Logger::info(cstring str)
 	SetConsoleTextAttribute(m_stderr, 10);
 	std::string string = "[INFO]";
 	string.append(str);
-	string.append(next_line);
+	string.append("\n");
 	puts(string.c_str());
 	SetConsoleTextAttribute(m_stderr, 0x07);
 }
@@ -28,7 +28,7 @@ void stdx::_Logger::warn(cstring str)
 	SetConsoleTextAttribute(m_stderr, 14);
 	std::string string = "[WARN]";
 	string.append(str);
-	string.append(next_line);
+	string.append("\n");
 	puts(string.c_str());
 	SetConsoleTextAttribute(m_stderr, 0x07);
 }
@@ -38,7 +38,7 @@ void stdx::_Logger::error(cstring str)
 	SetConsoleTextAttribute(m_stderr, 12|FOREGROUND_INTENSITY);
 	std::string string = "[ERROR]";
 	string.append(str);
-	string.append(next_line);
+	string.append("\n");
 	fputs(string.c_str(),stderr);
 	SetConsoleTextAttribute(m_stderr,0x07);
 }
@@ -48,7 +48,7 @@ void stdx::_Logger::debug(cstring str)
 	std::string string = "\033[1m\033[40;37m[DEBUG]";
 	string.append(str);
 	string.append("\033[39;49;0m");
-	string.append(next_line);
+	string.append("\n");
 	puts(string.c_str());
 }
 void stdx::_Logger::info(cstring str)
@@ -56,7 +56,7 @@ void stdx::_Logger::info(cstring str)
 	std::string string = "\033[1m\033[40;32m[INFO]";
 	string.append(str);
 	string.append("\033[39;49;0m");
-	string.append(next_line);
+	string.append("\n");
 	puts(string.c_str());
 }
 
@@ -65,7 +65,7 @@ void stdx::_Logger::warn(cstring str)
 	std::string string = "\033[1m\033[40;33m[WARN]";
 	string.append(str);
 	string.append("\033[39;49;0m");
-	string.append(next_line);
+	string.append("\n");
 	puts(string.c_str());
 }
 
@@ -74,7 +74,7 @@ void stdx::_Logger::error(cstring str)
 	std::string string = "\033[1m\033[40;31m[ERROR]";
 	string.append(str);
 	string.append("\033[39;49;0m");
-	string.append(next_line);
+	string.append("\n");
 	fputs(string.c_str(), stderr);
 }
 #endif
